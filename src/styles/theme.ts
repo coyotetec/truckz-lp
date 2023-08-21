ca5cfc9af702fcfc1bfc09f5039cfab2b65c55c2
Caleb Lima <lima.cardoso.caleb@gmail.com>
2023-08-21 15:45:52 -0300

    feat(style): create theme with colors

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+const theme = {
+  defaultColors: {
+    primary400: '#267233',
+
+    black300: '#313331',
+    black400: '#282928',
+    black500: '#1E1F1E',
+    black600: '#141414',
+
+    white100: '#FFFFFF',
+    white200: '#EDF0ED',
+    white300: '#DBE0DB',
+    white400: '#CDD1CD',
+  },
+};
+
+export default theme;
