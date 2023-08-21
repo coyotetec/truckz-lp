6133ceab1e2e634cefae678fe2ecb385bcf27659
Caleb Lima <lima.cardoso.caleb@gmail.com>
2023-08-21 18:21:51 -0300

    fix(header): fix position of navbar

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -2,11 +2,12 @@ import { styled } from 'styled-components';
 
 export const Container = styled.header`
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.black[500]};
+  background-color: ${({ theme }) => theme.colors.black[600]};
   display: flex;
   flex-direction: column;
   justify-content: center;
   height: 6rem;
+  position: fixed;
   width: 100%;
 `;
 
@@ -14,14 +15,16 @@ export const Content = styled.div`
   align-items: center;
   color: ${({ theme }) => theme.colors.white[100]};
   display: flex;
-  gap: 12rem;
   margin: 0 auto;
   max-width: 1120px;
+  width: 100%;
 `;
 
 export const Nav = styled.nav`
   display: flex;
   gap: 2.25rem;
+  width: 100%;
+  justify-content: center;
 
   li {
     list-style: none;
