695dc5049516eb58cb1f8915129891829938b959
Caleb Lima <lima.cardoso.caleb@gmail.com>
2023-08-21 16:15:57 -0300

    feat(style): create theme with colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { ThemeProvider } from 'styled-components';
 import { Header } from './components/Header';
 import { GlobalStyles } from './styles/GlobalStyles';
+import mainTheme from './styles/themes/mainTheme';
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <Header />
+      <ThemeProvider theme={mainTheme}>
+        <Header />
+        <GlobalStyles />
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/styles/themes/mainTheme.ts
@@ -0,0 +1,37 @@
+import { DefaultTheme } from 'styled-components';
+
+const mainTheme: DefaultTheme = {
+  colors: {
+    primary: {
+      100: '#D4E3D6',
+      200: '#9ABDA0',
+      300: '#609869',
+      400: '#267233',
+      500: '#205F2B',
+      600: '#194C22',
+      700: '#13391A',
+    },
+    black: {
+      50: '#545754',
+      100: '#454745',
+      200: '#3B3D3B',
+      300: '#313331',
+      400: '#282928',
+      500: '#1E1F1E',
+      600: '#141414',
+      700: '#0A0A0A',
+    },
+    white: {
+      100: '#FFFFFF',
+      200: '#EDF0ED',
+      300: '#DBE0DB',
+      400: '#CDD1CD',
+      500: '#BEC2BE',
+      600: '#AFB2AF',
+      700: '#A0A3A0',
+      800: '#7D807D',
+    },
+  },
+};
+
+export default mainTheme;
