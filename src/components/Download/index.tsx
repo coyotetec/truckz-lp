df5c0f4381dfe45ac9a5cfeb9a1451f2f20464ef
Caleb Lima <lima.cardoso.caleb@gmail.com>
2023-08-23 15:45:38 -0300

    feat(Download/Section): implememnts and style Download section

--- src/components/Download/index.tsx
+++ src/components/Download/index.tsx
@@ -1,11 +1,38 @@
-import { Container } from './style';
+import { Container, Content } from './style';
 
 import smartphones from '../../assets/smartphones.svg';
+import apple from '../../assets/apple-badge.svg';
+import google from '../../assets/google-badge.svg';
+import box from '../../assets/box.svg';
 
 export function Download() {
   return (
     <Container>
-      <img src={smartphones} alt="Smartphones exibindo a tela do truckz" />
+      <Content>
+        <img src={smartphones} alt="Smartphones exibindo a tela do truckz" />
+        <div>
+          <h1>Transporte inteligente ao seu alcance.</h1>
+          <h3>Instale o truckz e comece Já!</h3>
+          <img className="badge" src={apple} alt="Badge de download da Apple" />
+          <img
+            className="badge"
+            src={google}
+            alt="Badge de download do Google"
+          />
+          <div className="points">
+            <img src={box} alt="Ícone de caixa" />
+            <p>Encontre motoristas para suas cargas de forma rápida e fácil.</p>
+          </div>
+          <div className="points">
+            <img src={box} alt="Ícone de caixa" />
+            <p>Descubra cargas para serem transportadas.</p>
+          </div>
+          <div className="points">
+            <img src={box} alt="Ícone de caixa" />
+            <p>Faça uma seleção dos seus motoristas favoritos.</p>
+          </div>
+        </div>
+      </Content>
     </Container>
   );
 }
--- src/components/Download/style.ts
+++ src/components/Download/style.ts
@@ -1,7 +1,52 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
+  align-items: center;
   background-color: ${({ theme }) => theme.colors.black[400]};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   height: 100vh;
   padding-top: 6rem;
+`;
+
+export const Content = styled.div`
+  align-items: center;
+  display: flex;
+  gap: 4rem;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  max-width: 70rem;
+
+  h1 {
+    color: ${({ theme }) => theme.colors.white[100]};
+    font-size: 2rem;
+    max-width: 28rem;
+  }
+
+  h3 {
+    color: ${({ theme }) => theme.colors.white[400]};
+    font-size: 1.125rem;
+    font-weight: 300;
+    padding-bottom: 1.25rem;
+  }
+
+  .badge {
+    margin-bottom: 3.25rem;
+  }
+
+  .points {
+    align-items: center;
+    display: flex;
+    gap: 0.75rem;
+    margin-bottom: 0.875rem;
+
+    p {
+      color: ${({ theme }) => theme.colors.white[400]};
+      font-size: 1rem;
+      font-weight: 500;
+      max-width: 25.3125rem;
+    }
+  }
 `;
