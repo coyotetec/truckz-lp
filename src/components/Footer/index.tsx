4583cdad6dee9648926136979110b7de4ea8ea08
Caleb Lima <lima.cardoso.caleb@gmail.com>
2023-09-01 16:06:13 -0300

    chore(Footer): changes logo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,12 @@
 import { Container, Content } from './style';
 
-import logo from '../../assets/logo.svg';
+import logoFooter from '../../assets/logo-footer.svg';
 
 export function Footer() {
   return (
     <Container>
       <Content>
-        <img src={logo} alt="Logo do truckz" />
+        <img src={logoFooter} alt="Logo do truckz" />
         <p>Coyote Tecnologia © 2023 Todos os direitos reservados.</p>
       </Content>
     </Container>
