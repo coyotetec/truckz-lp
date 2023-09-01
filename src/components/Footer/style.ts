337457034acff613c67ae7f3ef44a49b794cbb15
Caleb Lima <lima.cardoso.caleb@gmail.com>
2023-09-01 16:07:41 -0300

    fix(Footer/style): adjustment in padding and color of text

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -11,11 +11,11 @@ export const Container = styled.footer`
 export const Content = styled.div`
   margin: 0 auto;
   max-width: 70rem;
-  padding: 0 1rem;
+  padding: 0 1.5rem;
   width: 100%;
 
   p {
-    color: ${({ theme }) => theme.colors.white[400]};
+    color: ${({ theme }) => theme.colors.white[800]};
     font-size: 0.875rem;
     font-weight: 100;
     padding-top: 0.5rem;
