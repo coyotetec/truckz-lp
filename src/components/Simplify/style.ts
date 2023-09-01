ada5b6e2451cd2e6501e49743005ff1eb49bf4ae
Caleb Lima <lima.cardoso.caleb@gmail.com>
2023-09-01 16:02:00 -0300

    fix(Simplify/Style): adjustments of section padding, style of section title and add before

--- src/components/Simplify/style.ts
+++ src/components/Simplify/style.ts
@@ -7,22 +7,24 @@ export const Container = styled.section`
   justify-content: center;
   height: auto;
   width: 100%;
+  padding: 4rem 0;
 `;
 
 export const Content = styled.div`
   margin: 0 auto;
   max-width: 70rem;
-  padding: 0 1rem;
+  padding: 0 1.5rem;
   width: 100%;
 
   h1 {
     color: ${({ theme }) => theme.colors.white[100]};
     font-size: 2rem;
-    font-weight: 400;
-    margin-bottom: 4.5rem;
+    font-weight: 700;
+    margin-bottom: 4rem;
     text-decoration: underline;
     text-decoration-color: ${({ theme }) => theme.colors.primary[400]};
-    text-underline-offset: 0.8rem;
+    text-underline-offset: 0.4rem;
+    text-align: center;
   }
 `;
 
@@ -32,7 +34,6 @@ export const Steps = styled.div`
   flex-wrap: wrap;
   gap: 6rem;
   justify-content: center;
-  padding-bottom: 4rem;
   width: 100%;
 
   div {
@@ -40,6 +41,23 @@ export const Steps = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
+    position: relative;
+    z-index: 4;
+
+    &::before {
+      content: '';
+      position: absolute;
+      width: 240px;
+      height: 240px;
+      background: ${({ theme }) => theme.colors.black[400]};
+      border-radius: 999px;
+      z-index: 4;
+    }
+
+    img {
+      height: 180px;
+      z-index: 5;
+    }
   }
 
   h2 {
@@ -47,5 +65,6 @@ export const Steps = styled.div`
     font-size: 24px;
     font-weight: 200;
     margin-bottom: 1.5rem;
+    z-index: 5;
   }
 `;
